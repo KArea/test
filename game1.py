212e71bb10f3ae61c8f3dfcf8a9cb0621a91c950
KArea <zkhlly@163.com>
2017-06-24 14:56:37 +0800

    小游戏
    猜数字小游戏

---
+++ game1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('---------------------我爱pythpn-------------------')
+temp = input('不妨猜一下我现在心里想的是哪个数字：')
+guess = int(temp)
+if guess == 8:
+    print('我艹，你是我肚子里的蛔虫吗？！')
+    print('哼，猜中了也没有奖励！')
+else:
+    print('猜错啦，我现在心里想的是8！')
+print('游戏结束，不玩啦^_^')
