33e4c544bc6c9876ff39abf43044b10b28c0a179
KArea <zkhlly@163.com>
2017-06-24 16:58:32 +0800

    改进小游戏
    优化循环和判断次数，三次机会的输入都在循环体内，“输入-判断”循环三次
    用while循环中的else语句实现正常退出后执行“三次都猜错啦！”

--- game1.py
+++ game1.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
-print('---------------我爱python-------------')
-guess = int(input('不妨猜一下我现在心里想的是哪个数字：'))
-n = 2
+print('---------------猜数游戏-------------')
+import random
+secret = random.randint(1,10)
+print('不妨猜一下我现在心里想的是哪个数字')
+n = 3
 while  n != 0:
-    n = n-1   
-    if guess == 8:
+    n = n-1
+    guess = int(input('请输入数字：'))
+    if guess == secret:
         print('我艹你是我肚子里的蛔虫吗？！')
         print('哼，猜中了也没有奖励！')
         break
-    else:
-        guess = int(input('哎呀，猜错了，请重新输入吧：'))
-        if guess > 8:
+    else:       
+        if guess > secret:
             print('哥，大了大了~~')
         else:
             print('嘿，小了！小了！！')
+else:
+    print('\n三次都猜错啦！')
 print('游戏结束，不玩啦^_^')
