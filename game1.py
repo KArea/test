9f9d681f7a937588755e0effa2603726b67dca89
KArea <zkhlly@163.com>
2017-06-24 16:34:05 +0800

    改进小游戏
    给玩家三次猜的机会，加入循环结构实现

--- game1.py
+++ game1.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 print('---------------我爱python-------------')
 guess = int(input('不妨猜一下我现在心里想的是哪个数字：'))
-if guess == 8:
-    print('我艹你是我肚子里的蛔虫吗？！')
-    print('哼，猜中了也没有奖励！')
-else:
-    if guess > 8:
-        print('哥，大了大了~~')
+n = 2
+while  n != 0:
+    n = n-1   
+    if guess == 8:
+        print('我艹你是我肚子里的蛔虫吗？！')
+        print('哼，猜中了也没有奖励！')
+        break
     else:
-        print('嘿，小了！小了！！')
+        guess = int(input('哎呀，猜错了，请重新输入吧：'))
+        if guess > 8:
+            print('哥，大了大了~~')
+        else:
+            print('嘿，小了！小了！！')
 print('游戏结束，不玩啦^_^')
