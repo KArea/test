1c5e4baf85d4794ad9e822c782882438f07dcf6a
KArea <zkhlly@163.com>
2017-06-26 14:25:19 +0800

    分类保存聊天记录小程序初步测试
    测试open的方法，将每一段聊天内容按“我说”、“他说”分别保存为两个文件夹，三段内容分别按序号保存

---
+++ test.py
@@ -0,0 +1,31 @@
+f = open('record.txt')
+
+boy = []
+girl = []
+count = 1
+
+for each_line in f:
+    if each_line[:6] != '======':
+        (role,line_spoken) = each_line.split('：',1)
+        if role == '我说':
+            boy.append(line_spoken)
+        if role == '他说':
+            girl.append(line_spoken)
+    else:
+        file_name_boy = 'boy_' + str(count) + '.txt'
+        file_name_girl = 'girl' + str(count) + '.txt'
+        
+        boy_file = open(file_name_boy,'w')
+        girl_file = open(file_name_girl,'w')
+        
+        boy_file.writelines(boy)
+        girl_file.writelines(girl)
+        
+        boy_file.close()
+        girl_file.close()
+        
+        boy = []
+        girl = []
+        count += 1
+        
+f.close()        
